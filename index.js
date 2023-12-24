ea6a4840d0001aae8d2256ed2f1c53f4295efc90
levanhoang2904 <levanhoang29042002@gmail.com>
2023-12-24 18:49:49 +0700

    update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ btn2.addEventListener('click', () => {
     
     box2[0].classList.add('hide')
     container[0].classList.remove('hide')
-    setInterval(makeSnow, 5000);l
+    setInterval(makeSnow, 5000);
 })
 const makeSnow = () => {
     const snow = document.createElement("div")
